ea8fd32d71db79f408a1cdb943926310d1d3c997
Joseph Petitti <joseph@petitti.org>
2021-05-28 12:23:15 -0400

    add valid-number

---
+++ valid-number.js
@@ -0,0 +1,9 @@
+/**
+ * https://leetcode.com/problems/valid-number/
+ *
+ * @param {string} s
+ * @return {boolean}
+ */
+var isNumber = function(s) {
+    return /^((([\+-]?((\d+\.)|(\d+\.\d+)|(\.\d+)))|([\+-]?\d+))([eE][\+-]?\d+)?)$/.test(s);
+};
